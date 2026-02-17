438034918161028e2b3a91a8ab6ac6220147009d
elvishn20 <elvis1042.rodriguez@gmail.com>
2026-02-16 23:19:49 -0600

    Estableciendo conexion con la base de datos

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/db');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send(`<h1>Inicio de prueba tecnica</h1>`);
 });
 
 app.listen(PORT, () => {
-    console.log(`Aplicacion funcional ${PORT}`)
+    console.log(`Aplicacion funcional ${PORT}`);
+
+    db.query('SELECT NOW()', (err, res) => {
+        if (err) {
+            console.error('Error en la conexion a Postgres:', err.message);
+        } else {
+            console.log('Base de datos conectada');
+        }
+    });
 })
---
+++ config/db.js
@@ -0,0 +1,23 @@
+const {Pool} = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+pool.on('connect', () => {
+    console.log('Conexion a Postgres en el puerto 5432');
+});
+
+pool.on('error', (err) => {
+    console.error('Error en la conexion con Postgres', err);
+    process.exit(-1);
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
